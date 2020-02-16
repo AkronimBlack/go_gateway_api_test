00100c0fba9808c1cfb3884642f6fb41f9b7781d
BlackBit <bojanmadzaracka@gmail.com>
2020-02-16 19:19:56 +0100

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/gateway
+
+go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/nautilus/gateway v0.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/nautilus/gateway"
+	"github.com/nautilus/graphql"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting introspect .... ")
+	schemas, err := graphql.IntrospectRemoteSchemas(
+		"http://localhost:8081/graphql",
+		//"http://localhost:8083",
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Creating gateway .... ")
+	gw, err := gateway.New(schemas)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Booting server .... ")
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Any("/graphql", func(context echo.Context) error {
+		gw.PlaygroundHandler(context.Response(), context.Request())
+		return nil
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
